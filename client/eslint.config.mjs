f5a5cd94b7515a2df33a3c9130dfc989683a454b
atanasivanovv <atanas.ivanov@dreamix.eu>
2024-10-04 17:17:58 +0300

    refactor: format eslint config

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -4,23 +4,27 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import prettier from "eslint-plugin-prettier";
 
+const config = {
+  plugins: {
+    prettier,
+  },
+
+  rules: {
+    "prettier/prettier": ["error"],
+  },
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
+
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {
-    plugins: {
-      prettier,
-    },
-    rules: {
-      "prettier/prettier": ["error"],
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
+  config,
 ];
