806d4c2479a98ff3bbe60b4cb983b800b39f5d1a
Atanas Ivanov <naskuuu123@gmail.com>
2024-10-10 14:43:11 +0300

    refactor: prefer gray color to primary in header

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -2,14 +2,14 @@ import { NavLink } from "react-router-dom";
 
 const getLinkClasses = ({ isActive }: { isActive: boolean }) =>
   isActive
-    ? "text-primary-400 font-bold hover:text-primary-600"
-    : "text-primary-400 hover:text-primary-600";
+    ? "text-gray-800 font-bold hover:text-black"
+    : "text-gray-800 hover:text-black";
 
 const Header = () => {
   return (
     <header className="bg-white shadow-md">
       <nav className="flex justify-between mx-auto px-8 py-4">
-        <h2 className="text-xl text-primary-500">
+        <h2 className="text-xl text-gray-800">
           <i className="fa-solid fa-suitcase mr-2" />
           Users Back Office
         </h2>
